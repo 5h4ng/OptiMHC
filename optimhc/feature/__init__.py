4901ae674dcc2c55a8fbc8e00bf381fbb48b990c
Zixiang Shang <steven5h4ng@gmail.com>
2026-03-15 23:56:54 +0800

    Refactor feature generation logic

--- optimhc/feature/__init__.py
+++ optimhc/feature/__init__.py
@@ -1,3 +1,27 @@
-import warnings
+from optimhc.feature.base_feature_generator import BaseFeatureGenerator
+from optimhc.feature.basic import BasicFeatureGenerator
+from optimhc.feature.deeplc import DeepLCFeatureGenerator
+from optimhc.feature.factory import feature_generator_factory
+from optimhc.feature.mhcflurry import MHCflurryFeatureGenerator
+from optimhc.feature.netmhciipan import NetMHCIIpanFeatureGenerator
+from optimhc.feature.netmhcpan import NetMHCpanFeatureGenerator
+from optimhc.feature.overlapping_peptide import (
+    OverlappingPeptideFeatureGenerator,
+)
+from optimhc.feature.pwm import PWMFeatureGenerator
+from optimhc.feature.spectral_similarity import (
+    SpectralSimilarityFeatureGenerator,
+)
 
-warnings.filterwarnings("ignore")
+__all__ = [
+    "feature_generator_factory",
+    "BaseFeatureGenerator",
+    "BasicFeatureGenerator",
+    "PWMFeatureGenerator",
+    "OverlappingPeptideFeatureGenerator",
+    "MHCflurryFeatureGenerator",
+    "NetMHCpanFeatureGenerator",
+    "NetMHCIIpanFeatureGenerator",
+    "DeepLCFeatureGenerator",
+    "SpectralSimilarityFeatureGenerator",
+]
